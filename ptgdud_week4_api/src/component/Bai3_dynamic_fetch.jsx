c656c85918a9899d6124179b7552a01f7ea0eb9e
ManHuynhSE <manchuadu18@gmail.com>
2026-03-12 13:47:45 +0700

    "init"

--- ptgdud_week4_api/src/component/Bai3_dynamic_fetch.jsx
+++ ptgdud_week4_api/src/component/Bai3_dynamic_fetch.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Bai3_dynamic_fetch() {
-    const [user, setUser] = useState(null);
-    const [idFind, setIdFind] = useState('1');
-    const [error, setError] = useState(null)
-    useEffect(() => {
-        N`   `
-
-
-
-        async function fetchId() {
-            try {
-                if (!fetchApi.ok) {
-                    throw new Error("Network..")
-                }
-                var fetchApi = await fetch(`https://jsonplaceholder.typicode.com/users/${idFind}`)
-                var rs = await fetchApi.json()
-                console.log(rs);
-                setUser(rs);
-            }
-            catch (error) {
-                console.log("dsds");
-                setError(error.message)
-            }
-
+  const [user, setUser] = useState(null);
+  const [idFind, setIdFind] = useState("1");
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    async function fetchId() {
+      setError(null);
+      try {
+        var fetchApi = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${idFind}`,
+        );
+        if (!fetchApi.ok) {
+          throw new Error("Network..");
         }
+        var rs = await fetchApi.json();
+        console.log(rs);
+        setUser(rs);
+      } catch (error) {
+        console.log("dsds");
+        setError(error.message);
+      }
+    }
 
-        fetchId()
-    }, [idFind])
-    return <>
-        <div>
-            <input value={idFind} type="text" onChange={(e) => setIdFind(e.target.value)} />
-            {
-                error === null ? (
-                    user === null ? (
-                        <p>No such user</p>
-                    ) : (
-                        <p>{user.name}</p>
-                    )
-                ) : (
-                    <p>No such user</p>
-
-                )
-            }
-        </div>
+    fetchId();
+  }, [idFind]);
+  return (
+    <>
+      <div>
+        <input
+          value={idFind}
+          type="text"
+          onChange={(e) => setIdFind(e.target.value)}
+        />
+        {error === null ? (
+          user === null ? (
+            <p>No such user</p>
+          ) : (
+            <p>{user.name}</p>
+          )
+        ) : (
+          <p>No such user</p>
+        )}
+      </div>
     </>
+  );
 }
 
-export default Bai3_dynamic_fetch
+export default Bai3_dynamic_fetch;
